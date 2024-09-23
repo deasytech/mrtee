64c59822499df2a16de529e32b7a4a07c40b928c
Desmond Awere <desmondawere@gmail.com>
2024-09-23 18:32:50 +0100

    footer updated

--- components/frontend/layout/footer.tsx
+++ components/frontend/layout/footer.tsx
@@ -1,27 +1,64 @@
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className="bg-red-50 py-10 px-10 flex flex-col max-sm:px-2 mt-16">
-      <div className="flex flex-wrap py-8 gap-16 items-start justify-center">
-        <Link href="/">
-          <Image src="/images/logo.png" alt="logo" width={150} height={59} />
-        </Link>
-        <div className="space-y-3">
-          <p className="text-heading3-bold">Contact</p>
-          <p className="text-base-bold">+234 703 292 0367</p>
-          <p className="text-base-bold">hello@mrtee.com</p>
-        </div>
-        <div className="space-y-3">
-          <p className="text-heading3-bold">Never Miss a Deal</p>
-          <div className="flex gap-2">
-            <Input className="bg-transparent border-gray-700 rounded-none" />
-            <Button type="button" className="rounded-none">Subscribe</Button>
+    <div className="bg-black text-white py-10 px-10 flex flex-col max-sm:px-5 mt-16">
+      <div className="flex flex-col md:flex-row gap-6 w-full py-8">
+        <div className='flex flex-wrap gap-6 items-start w-full md:w-1/4'>
+          <div className='flex flex-col'>
+            <Link href="/" className='mb-2'>
+              <Image src="/images/logo.png" alt="logo" width={150} height={59} />
+            </Link>
+            <p className="text-gray-400 text-small-medium">Welcome to Mr Tee, your ultimate destination for luxury men's fashion. We believe that style is a reflection of individuality, and our curated selection of high-end fashion pieces is designed to help you make a statement.</p>
           </div>
-          <p className="text-xs">Join our subscribers and get best deals delivered each week!</p>
+          <div className="space-y-3">
+            <p className="text-small-medium">Got Question? Call us 24/7</p>
+            <p className="text-base-bold text-gold">+234 703 292 0367</p>
+          </div>
+        </div>
+        <div className='flex flex-wrap justify-between items-start w-full md:w-3/4'>
+          <div>
+            <h3 className="uppercase text-white mb-3">Useful Links</h3>
+            <ul className="text-gray-400 space-y-2">
+              <li><Link href="#" className="hover:text-gold">About Mr Tee Fashion Store</Link></li>
+              <li><Link href="#" className="hover:text-gold">How to shop on Mr Tee</Link></li>
+              <li><Link href="#" className="hover:text-gold">FAQ</Link></li>
+              <li><Link href="#" className="hover:text-gold">Contact us</Link></li>
+              <li><Link href="/login" className="hover:text-gold">Login</Link></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="uppercase text-white mb-3">Customer Service</h3>
+            <ul className="text-gray-400 space-y-2">
+              <li><Link href="#" className="hover:text-gold">Payment Methods</Link></li>
+              <li><Link href="#" className="hover:text-gold">Money-back guarantee!</Link></li>
+              <li><Link href="#" className="hover:text-gold">Returns</Link></li>
+              <li><Link href="#" className="hover:text-gold">Shipping</Link></li>
+              <li><Link href="#" className="hover:text-gold">Terms and conditions</Link></li>
+              <li><Link href="#" className="hover:text-gold">Privacy Policy</Link></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="uppercase text-white mb-3">My Account</h3>
+            <ul className="text-gray-400 space-y-2">
+              <li><Link href="#" className="hover:text-gold">Register</Link></li>
+              <li><Link href="#" className="hover:text-gold">View Cart</Link></li>
+              <li><Link href="#" className="hover:text-gold">My Wishlist</Link></li>
+              <li><Link href="#" className="hover:text-gold">Track My Order</Link></li>
+              <li><Link href="#" className="hover:text-gold">Help</Link></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="uppercase text-white mb-3">Sign up to newsletter</h3>
+            <p className="text-gray-400 text-small-medium mb-2">Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan</p>
+            <Input className="bg-white text-gray-800 border-gray-700 rounded-none" placeholder="Enter your email address" />
+            <div className="space-y-3 mt-6">
+              <p className="text-small-medium">Got Question? Call us 24/7</p>
+              <Image src="/images/payments.png" alt="payment" width={272} height={20} />
+            </div>
+          </div>
         </div>
       </div>
       <div className="w-full px-10 my-5 border-b border-dashed border-gray-500" />
