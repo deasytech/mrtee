4128993333428469448d0f2180ef6602f2f1c389
Desmond Awere <desmondawere@gmail.com>
2024-11-12 13:42:10 +0100

    fix deployment error

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
+export async function generateStaticParams() {
+  return [
+    { slug: [] },
+  ];
+}
+
 export default function Page() {
   return <SignUp />;
 }
